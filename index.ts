164944c19ad070dbe379d9d688ed2a554bb64519
Matthew Skillington <matthewskillington14@gmail.com>
2024-01-16 16:32:38 +0000

    fix: update entry point for cloud functions

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import { sendNotification } from "./src/sendNotification.js";
 
 const url = "https://usedcars.bmw.co.uk/result/?max_supplied_price=28000&min_age=4&min_supplied_price=15000&series=1+Series&series=3+Series&sort=-placement_date&variant=140i&variant=340i&variant=320i&variant=330i"
 
-const entry = async () => {
+export const entry = async () => {
     const browser = await puppeteer.launch({
         headless: true,
         defaultViewport: null,
@@ -36,4 +36,4 @@ const entry = async () => {
 
 }
 
-entry()
+// entry() 
