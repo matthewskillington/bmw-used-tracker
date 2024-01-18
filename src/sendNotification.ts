ae0c2d204d48a6decfc3f63fd8ebc5ea641316a6
Matthew Skillington <matthewskillington14@gmail.com>
2024-01-18 10:46:49 +0000

    fix: formatting

--- src/sendNotification.ts
+++ src/sendNotification.ts
@@ -1,4 +1,4 @@
-const webhookUrl = "https://discord.com/api/webhooks/1196172434726993990/HLTiuvLjSOPjDrmXRv1U6_dCQJk0DGI5TaG3B4DeFK_nBhpgYNu6yYLTkwRA_AObXcM2";
+const webhookUrl = "";
 
 export const sendNotification = (payload: string) => {
     const data = { content: payload }
